8440bf23a0f338183c77a1dc1e6378c90c43b31c
lifez-data-ai <jiachenzhu2020@gmail.com>
2024-06-05 15:05:16 -0400

    pinecone api fixes

--- simple-app.py
+++ simple-app.py
@@ -136,8 +136,9 @@ embedding_function = VoyageAIEmbeddings(
     voyage_api_key=VOYAGE_AI_API_KEY
 )
 vector_store = PineconeVectorStore(
-    pinecone_api_key=PINECONE_API_KEY,
-    embedding=embedding_function
+        pinecone_api_key=PINECONE_API_KEY,
+        embedding=embedding_function,
+        index_name=index_name
     ).from_existing_index(
         embedding=embedding_function,
         index_name=index_name
